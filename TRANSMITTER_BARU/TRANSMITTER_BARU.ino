76bec1d0b17ff9544a9afa02a6cd8f53da40ab84
Yohanes Andre Setiawan <andre44s.main@gmail.com>
2021-08-05 18:43:16 +0800

    Added Code to GitHub

--- TRANSMITTER_BARU/TRANSMITTER_BARU.ino
+++ TRANSMITTER_BARU/TRANSMITTER_BARU.ino
@@ -1,19 +1,51 @@
+int s0 = 17;
+int s1 = 16;
+int s2 = 4;
+int s3 = 2;
+
+String s0Out;
+String s1Out;
+String s2Out;
+String s3Out;
+
+int z = 13;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(s0, OUTPUT);
+  pinMode(s1, OUTPUT);
+  pinMode(s2, OUTPUT);
+  pinMode(s3, OUTPUT);
+  pinMode(z, INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int num = 1234;
+  for (int o = 0; o < 16; o++) {
 
-  uint8_t bitsCount = sizeof( num ) * 8;
-  char str[ bitsCount + 1 ];
+    uint8_t bitsCount = sizeof( o );
+    char str[ bitsCount + 1 ];
 
-  uint8_t i = 0;
-  while ( bitsCount-- )
-    str[ i++ ] = bitRead( num, bitsCount ) + '0';
-  str[ i ] = '\0';
+    uint8_t i = 0;
+    while ( bitsCount-- )
+      str[ i++ ] = bitRead( o, bitsCount ) + '0';
+    str[ i ] = '\0';
 
-  Serial.println( str );
+    s0Out = String(str[0]);
+    s1Out = String(str[1]);
+    s2Out = String(str[2]);
+    s3Out = String(str[3]);
+
+    digitalWrite(s0, LOW);
+    digitalWrite(s1, LOW);
+    digitalWrite(s2, LOW);
+    digitalWrite(s3, HIGH);
+
+    Serial.print(o);
+    Serial.print(" = ");
+    Serial.println(analogRead(z));
+    
+    delay(500);
+  }
 }
